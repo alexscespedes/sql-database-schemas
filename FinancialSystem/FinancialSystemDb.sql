3984f632b653b46c6215e1ff0f0c78c0c02a01ce
Alexander Sencion Cespedes <alexandersenc99@gmail.com>
2025-08-23 13:22:32 -0400

    Project 2 Relationships & Constraints: Small Financial System (DB)

--- FinancialSystem/FinancialSystemDb.sql
+++ FinancialSystem/FinancialSystemDb.sql
@@ -31,7 +31,7 @@ CREATE TABLE Loans (
     ClientID INT NOT NULL,
     PrincipalAmount DECIMAL(12,2) NOT NULL,
     InterestRate DECIMAL(5,2) NOT NULL,
-    TermsMonths INT NOT NULL,
+    TermMonths INT NOT NULL,
     StartDate DATE NOT NULL,
     Status NVARCHAR(20) CHECK (Status IN ('Active', 'Closed', 'Overdue')) DEFAULT 'Active',
     CreatedBy INT NOT NULL,
@@ -64,3 +64,80 @@ CREATE TABLE AuditLogs (
     Timestamp DATETIME DEFAULT GETDATE(),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
+
+--- PHASE 2: RELATIONSHIPS & CONSTRAINTS
+
+-- 1.
+ALTER TABLE Clients
+ADD CONSTRAINT UQ_Client UNIQUE (FirstName, LastName, DateOfBirth);
+
+-- 2.
+ALTER TABLE Users
+ADD UNIQUE (Username); 
+
+-- 3.
+
+ALTER TABLE Loans
+ADD CONSTRAINT CK_Loans_Principal CHECK (PrincipalAmount > 0);
+
+ALTER TABLE Loans
+ADD CONSTRAINT CK_Loans_Interest CHECK (InterestRate >= 0);
+
+ALTER TABLE Loans
+ADD CONSTRAINT CK_Loans_Term CHECK (TermMonths >= 1);
+
+EXEC sp_rename 'dbo.Loans.TermsMonths', 'TermMonths', 'COLUMN';
+
+ALTER TABLE Loans
+ADD CONSTRAINT DF_Loan_Status DEFAULT 'Active' FOR Status;
+
+-- 4.
+
+ALTER TABLE Payments
+ADD CONSTRAINT CK_Payments_Amount CHECK (Amount > 0);
+GO
+
+--- TRIGGER ---
+
+CREATE TRIGGER TR_ValidatePayment
+ON Payments
+INSTEAD OF INSERT
+AS
+BEGIN
+    -- Check for payments on closed/overdue loans
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN Loans l ON i.LoanID = l.LoanID
+        WHERE l.Status <> 'Active'
+    )
+    BEGIN
+        RAISERROR('Cannot add payment to a closed or overdue loan.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END;
+
+    -- Check for overpayments
+     IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN Loans l ON i.LoanID = l.LoanID
+        CROSS APPLY (
+            SELECT ISNULL(SUM(p.Amount),0) AS TotalPaid
+            FROM Payments p
+            WHERE p.LoanID = l.LoanID
+        ) t
+        WHERE (t.TotalPaid + i.Amount) >
+              (l.PrincipalAmount + (l.PrincipalAmount * l.InterestRate/100))
+    )
+    BEGIN
+        RAISERROR('Payment exceeds remaining balance.', 16, 1);
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END;
+
+    INSERT INTO Payments (LoanID, Amount, PaymentDate, RecordedBy)
+    SELECT LoanID, Amount, PaymentDate, RecordedBy
+    FROM inserted;
+END;
+GO
